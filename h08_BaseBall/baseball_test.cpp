7fa6ab2441850ae867ddbe0042a887abef86998c
wanny <wankims@gmail.com>
2025-04-11 16:17:06 +0900

    [feature] page 30

--- h08_BaseBall/baseball_test.cpp
+++ h08_BaseBall/baseball_test.cpp
@@ -1,10 +1,16 @@
 #include "gmock/gmock.h"
 #include "baseball.cpp"
 
-TEST(BaseballGame, TryGameTest) {
+TEST(BaseballGame, ThrowException) {
 	BaseBall game;
 	EXPECT_THROW(game.guess(string("12")), length_error);
 }
+
+TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
+	BaseBall game;
+	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+}
+
 
 int main()
 {
